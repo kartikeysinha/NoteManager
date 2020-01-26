33fee9d46a2ff07bbbe2047fa13d57393e7fd650
kartikeysinha <7ktk10@gmail.com>
2020-01-25 22:41:08 -0500

    Edit and Delete Notes

--- notemaking.js
+++ notemaking.js
@@ -1,5 +1,7 @@
 var ul = document.querySelector('ul');
 
+
+//******************** ADD ITEMS ********************//
 document.getElementById('add-btn').addEventListener('click', 
 function (e) {
     e.preventDefault();
@@ -8,30 +10,72 @@ function (e) {
 
 
     if (addInput.value !== '') {
-    var li = document.createElement('li'),
-        firstPar = document.createElement('p'),
-        secondPar = document.createElement('p'),
-        firstIcon = document.createElement('i'),
-        secondIcon = document.createElement('i'),
-        input = document.createElement('input');
+        var li = document.createElement('li'),
+            firstPar = document.createElement('p'),
+            secondPar = document.createElement('p'),
+            firstIcon = document.createElement('i'),
+            secondIcon = document.createElement('i'),
+            input = document.createElement('input');
 
-    firstIcon.className = "fa fa-pencil-square-o";
-    secondIcon.className = "fa fa-times";
+        firstIcon.className = "fa fa-pencil-square-o";
+        secondIcon.className = "fa fa-times";
 
-    input.className = "edit-note";
-    input.setAttribute('type', 'text');
+        input.className = "edit-note";
+        input.setAttribute('type', 'text');
 
-    firstPar.textContent = addInput.value;
-    secondPar.appendChild(firstIcon);
-    secondPar.appendChild(secondIcon);
+        firstPar.textContent = addInput.value;
+        secondPar.appendChild(firstIcon);
+        secondPar.appendChild(secondIcon);
 
-    li.appendChild(firstPar);
-    li.appendChild(secondPar);
-    li.appendChild(input);
+        li.appendChild(firstPar);
+        li.appendChild(secondPar);
+        li.appendChild(input);
 
-    ul.appendChild(li);
+        ul.appendChild(li);
 
-    addInput.value = '';
+        addInput.value = '';
     }
 
 });
+
+//******************** EDIT AND DELETE ITEMS ********************//
+
+ul.addEventListener('click', function (e) {
+    if (e.target.classList[1] === "fa-pencil-square-o") {
+
+        var parentPar = e.target.parentNode;
+        parentPar.style.display = 'none';
+
+        var note = parentPar.previousElementSibling;
+        var input = parentPar.nextElementSibling;
+
+        input.style.display = 'block';
+        input.value = note.textContent;
+
+        input.addEventListener('keypress', function (e) {
+            console.log(e);
+            if (e.keyCode === 13) {
+                if (input.value !== '') {
+                    note.textContent = input.value;
+                    parentPar.style.display = 'block';
+                    input.style.display = 'none';
+                } else {
+                    var li = input.parentNode;
+                    li.parentNode.removeChild(li);
+                }
+            }
+        });
+
+    } else if (e.target.classList[1] === "fa-times") {
+
+        var list = e.target.parentNode.parentNode;
+        list.parentNode.removeChild(list);
+
+    }
+})
+
+
+//******************** HIDE NOTES ********************//
+
+
+//******************** SEARCH NOTES ********************//
