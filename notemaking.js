57607581bb9449ac5abdde8c9d6a72f51bd3cab1
kartikeysinha <7ktk10@gmail.com>
2020-01-25 23:06:50 -0500

    search

--- notemaking.js
+++ notemaking.js
@@ -96,3 +96,23 @@ hideItem.addEventListener('click', function() {
 
 //******************** SEARCH NOTES ********************//
 
+var searchInput = document.querySelector('#search-note input');
+
+searchInput.addEventListener('keyup', function(e) {
+
+    var searchChar = e.target.value.toUpperCase();
+
+    var notes = ul.getElementsByTagName('li');
+
+    Array.from(notes).forEach(function (note) {
+        var parText = note.firstElementChild.textContent;
+
+        if (parText.toUpperCase().indexOf(searchChar) !== -1) {
+            note.style.display = 'block';
+        } else {
+            note.style.display = 'none';
+        }
+
+    })
+
+})
